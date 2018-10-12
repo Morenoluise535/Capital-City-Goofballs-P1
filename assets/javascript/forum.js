dbaf647dc23f110facf89dacfbd83edf141b9ec5
Luis Morneno <luismorneno@Luiss-MacBook-Pro.local>
2018-10-11 21:01:22 -0700

    now addsmessages

--- assets/javascript/forum.js
+++ assets/javascript/forum.js
@@ -1,11 +1,38 @@
 $( document ).ready(function() {
 
-$(".reply").hide()
+var mainmessagearr = []
+console.log(mainmessagearr)
 
 
-$(".replybtn").on('click', function(){
-    
-    $(".reply").show()
-}) 
+
+$(".sendbtnprim").on('click', function(event){
+    event.preventDefault()
+
+    var chatmessage = $("<div class='conformess'>"+
+                        "<p>"+ name + "</p>"+
+                        "<p>" + message+ "</p>"+
+                        "<div class='replymessage'></div>"+
+                        "<button class='replybtn'>Reply</button>"+
+                        "<div class='replyform'></div>"+
+                        "</div>")
+
+    var name = $("#name-input").val().trim();
+    var message = $("#message-input").val().trim();
+
+
+    $(".formess").append(chatmessage)
+    mainmessagearr.push(chatmessage)
+
+    console.log(name)
+    console.log(message)
+
+    $(".replybtn").on('click', function(){
+
+
+        console.log("hi")
+    })
+
+})
+
 
 });
